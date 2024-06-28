b53a43feb42862b46b97e31c02b5ee196b09bebe
Andrei Galan <galan.andrei@gmail.com>
2024-06-28 12:41:15 +0300

    Implement CRUD endpoints

---
+++ GameStore.API/Dtos/CreateGameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.API.Dtos;
+
+public record class CreateGameDto(
+    string Name, 
+    string Genre, 
+    decimal Price, 
+    DateOnly ReleaseDate
+);
---
+++ GameStore.API/Dtos/GameDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.API.Dtos;
+
+public record class GameDto(
+    int Id, 
+    string Name, 
+    string Genre, 
+    decimal Price, 
+    DateOnly ReleaseDate
+);
+
