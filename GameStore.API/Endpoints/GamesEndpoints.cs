e582faa195daf7a611c7c85efb02cada03c07d98
Andrei Galan <galan.andrei@gmail.com>
2024-06-29 17:24:22 +0300

    Saving new entitites to the database

--- GameStore.API/Endpoints/GamesEndpoints.cs
+++ GameStore.API/Endpoints/GamesEndpoints.cs
@@ -1,4 +1,6 @@
-﻿using GameStore.API.Dtos;
+﻿using GameStore.API.Data;
+using GameStore.API.Dtos;
+using GameStore.API.Entities;
 
 namespace GameStore.API.Endpoints;
 
@@ -45,18 +47,29 @@ public static class GamesEndpoints
         .WithName(GetGameEndpointName);
 
         // POST /games
-        group.MapPost("/", (CreateGameDto newGame) => 
+        group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) => 
         {
-            GameDto game = new(
-                games.Count + 1,
-                newGame.Name,
-                newGame.Genre,
-                newGame.Price,
-                newGame.ReleaseDate);
+            Game game = new()
+            {
+                Name = newGame.Name,
+                Genre = dbContext.Genres.Find(newGame.GenreId),
+                GenreId = newGame.GenreId,
+                Price = newGame.Price,
+                ReleaseDate = newGame.ReleaseDate
+            };
 
-            games.Add(game);
+            dbContext.Games.Add(game);
+            dbContext.SaveChanges();
 
-            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
+            GameDto gameDto = new(
+                game.Id,
+                game.Name,
+                game.Genre!.Name,
+                game.Price,
+                game.ReleaseDate
+            );
+
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, gameDto);
         });
 
         // PUT /games
