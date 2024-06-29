a480d77bbede0f151d6769e94b2c0de9f1ad1307
Andrei Galan <galan.andrei@gmail.com>
2024-06-29 13:24:41 +0300

    Using Configuration system

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -3,7 +3,7 @@ using GameStore.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "Data Source=GameStore.db";
+var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
