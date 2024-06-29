e4f970c5a9264b69cebd93996bb6dae82f8b0d4c
Andrei Galan <galan.andrei@gmail.com>
2024-06-29 13:12:02 +0300

    Defining the data model

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,6 +1,11 @@
+using GameStore.API.Data;
 using GameStore.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
