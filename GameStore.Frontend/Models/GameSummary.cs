2ccf07094298dc42d20106ad1fe34750362ff76f
Andrei Galan <galan.andrei@gmail.com>
2024-07-02 09:45:39 +0300

    Using Razor syntax

---
+++ GameStore.Frontend/Models/GameSummary.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.Frontend.Models;
+
+public class GameSummary
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+}
