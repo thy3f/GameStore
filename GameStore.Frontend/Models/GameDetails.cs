eb2cc98519b1220f30610253d33e9f22fd9e153e
Andrei Galan <galan.andrei@gmail.com>
2024-07-03 14:01:42 +0300

    Use JSON serialization converter

--- GameStore.Frontend/Models/GameDetails.cs
+++ GameStore.Frontend/Models/GameDetails.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using GameStore.Frontend.Converters;
 
 namespace GameStore.Frontend.Models;
 
@@ -11,6 +13,7 @@ public class GameDetails
     public required string Name { get; set; }
 
     [Required(ErrorMessage = "The Genre field is required.")]
+    [JsonConverter(typeof(StringConverter))]
     public string? GenreId { get; set; }
 
     [Range(1, 100)]
